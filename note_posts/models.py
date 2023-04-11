e945e0a613010930bc93ec04f503b7a6c025e96a
Tanmoy Saha <tanmoytssaha@gmail.com>
2023-04-12 00:26:22 +0530

    Working comment logic

--- note_posts/models.py
+++ note_posts/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     post_liked = models.PositiveIntegerField(default=0)
 
     def __str__(self):
-        return str(self.post_id) + '. ' + self.post_content[:20]
+        return str(self.post_id) + '. ' + self.post_content[:20] + '...'
     
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk':self.pk})
@@ -29,6 +29,7 @@ class Like(models.Model):
         return f'{self.user.username} likes {self.post.post_content[:20]}...'
     
 class Comment(models.Model):
+    comment_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     comment_content = models.TextField(max_length=1000, null=False, blank=False)
--- note_posts/urls.py
+++ note_posts/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import PostListView, PostDetailedView, PostLikeAPIView
+from .views import PostListView, PostDetailedView, PostLikeAPIView, CommentAPIView, UserPostAPIView, CommentGetAPIView, CommentPutDeleteAPIView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='posts'),
     path('post=<int:post_id>/', PostDetailedView.as_view(), name='post_detail'),
-    path('user=<int:user_id>/', PostListView.as_view(), name='user_posts'),
-    path('post=<int:post_id>/like', PostLikeAPIView.as_view(), name='post-like')
+    path('user=<int:user_id>/', UserPostAPIView.as_view(), name='user_posts'),
+    path('post=<int:post_id>/like/', PostLikeAPIView.as_view(), name='post-like'),
+    path('comment/create/', CommentAPIView.as_view(), name='comment-POST'),
+    path('comment/post=<int:post_id>/', CommentGetAPIView.as_view(), name='comment-GET'),
+    path('comment/delete=<int:comment_id>/', CommentPutDeleteAPIView.as_view(), name='comment-DELETE'),
+    path('comment/update=<int:comment_id>/', CommentPutDeleteAPIView.as_view(), name='comment-UPDATE'),
 ]
