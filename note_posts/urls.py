022fd65704673cab225c9acd769f529feb23781d
Tanmoy Saha <tanmoytssaha@gmail.com>
2023-04-23 01:23:17 +0530

    changes in comment url and like view

--- note_posts/urls.py
+++ note_posts/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('post=<int:post_id>/', PostDetailedView.as_view(), name='post_detail'),
     path('user=<int:user_id>/', UserPostAPIView.as_view(), name='user_posts'),
     path('post=<int:post_id>/like/', PostLikeAPIView.as_view(), name='post-like'),
+    path('comment/', CommentAPIView.as_view(), name='comment-GET'),
     path('comment/create/', CommentAPIView.as_view(), name='comment-POST'),
     path('comment/post=<int:post_id>/', CommentGetAPIView.as_view(), name='comment-GET'),
     path('comment/delete=<int:comment_id>/', CommentPutDeleteAPIView.as_view(), name='comment-DELETE'),
